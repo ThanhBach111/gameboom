cbabec70ad44adaea346e7aa4050e2aa071116f5
duongyp23 <71799659+duongyp23@users.noreply.github.com>
2020-11-15 23:48:27 +0700

    up

---
+++ src/uet/oop/bomberman/graphics/Sprite.java
@@ -0,0 +1,130 @@
+package uet.oop.bomberman.graphics;
+
+import javafx.scene.image.*;
+import sun.security.provider.ConfigFile;
+
+import java.awt.image.BufferedImage;
+import java.nio.IntBuffer;
+
+/**
+ * Lưu trữ thông tin các pixel của 1 sprite (hình ảnh game)
+ */
+public class Sprite {
+	
+	public static final int DEFAULT_SIZE = 32;
+	public static final int SCALED_SIZE = DEFAULT_SIZE * 2;
+    private static final int TRANSPARENT_COLOR = 0xffff00ff;
+	public final int SIZE;
+	private int _x, _y;
+	public int[] _pixels;
+	protected int _realWidth;
+	protected int _realHeight;
+	private SpriteSheet _sheet;
+
+	public static Sprite captain = new Sprite(DEFAULT_SIZE, 0, 0, SpriteSheet.captain, 0,0);
+	public static Sprite wall = new Sprite(DEFAULT_SIZE, 0, 0, SpriteSheet.wall_, 0,0);
+	public static Sprite grass = new Sprite(DEFAULT_SIZE, 0, 0, SpriteSheet.grass_, 0,0);
+	
+	public Sprite(int size, int x, int y, SpriteSheet sheet, int rw, int rh) {
+		SIZE = size;
+		_pixels = new int[SIZE * SIZE];
+		_x = x * SIZE;
+		_y = y * SIZE;
+		_sheet = sheet;
+		_realWidth = rw;
+		_realHeight = rh;
+		load();
+	}
+	
+	public Sprite(int size, int color) {
+		SIZE = size;
+		_pixels = new int[SIZE * SIZE];
+		setColor(color);
+	}
+	
+	private void setColor(int color) {
+		for (int i = 0; i < _pixels.length; i++) {
+			_pixels[i] = color;
+		}
+	}
+
+	private void load() {
+		for (int y = 0; y < SIZE; y++) {
+			for (int x = 0; x < SIZE; x++) {
+				_pixels[x + y * SIZE] = _sheet._pixels[(x + _x) + (y + _y) * _sheet.SIZE];
+			}
+		}
+	}
+	
+	public static Sprite movingSprite(Sprite normal, Sprite x1, Sprite x2, int animate, int time) {
+		int calc = animate % time;
+		int diff = time / 3;
+		
+		if(calc < diff) {
+			return normal;
+		}
+			
+		if(calc < diff * 2) {
+			return x1;
+		}
+			
+		return x2;
+	}
+	
+	public static Sprite movingSprite(Sprite x1, Sprite x2, int animate, int time) {
+		int diff = time / 2;
+		return (animate % time > diff) ? x1 : x2; 
+	}
+	
+	public int getSize() {
+		return SIZE;
+	}
+
+	public int getPixel(int i) {
+		return _pixels[i];
+	}
+
+	public Image getFxImage() {
+        WritableImage wr = new WritableImage(SIZE, SIZE);
+        PixelWriter pw = wr.getPixelWriter();
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if ( _pixels[x + y * SIZE] == TRANSPARENT_COLOR) {
+                    pw.setArgb(x, y, 0);
+                }
+                else {
+                    pw.setArgb(x, y, _pixels[x + y * SIZE]);
+                }
+            }
+        }
+        Image input = new ImageView(wr).getImage();
+        return resample(input, SCALED_SIZE / DEFAULT_SIZE);
+    }
+
+	private Image resample(Image input, int scaleFactor) {
+		final int W = (int) input.getWidth();
+		final int H = (int) input.getHeight();
+		final int S = scaleFactor;
+
+		WritableImage output = new WritableImage(
+				W * S,
+				H * S
+		);
+
+		PixelReader reader = input.getPixelReader();
+		PixelWriter writer = output.getPixelWriter();
+
+		for (int y = 0; y < H; y++) {
+			for (int x = 0; x < W; x++) {
+				final int argb = reader.getArgb(x, y);
+				for (int dy = 0; dy < S; dy++) {
+					for (int dx = 0; dx < S; dx++) {
+						writer.setArgb(x * S + dx, y * S + dy, argb);
+					}
+				}
+			}
+		}
+
+		return output;
+	}
+}
