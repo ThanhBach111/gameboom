d5753b954f28f66c033986050d765696d7d6a9bb
ThanhBach111 <thanhbachblo2@gmail.com>
2020-11-25 19:35:29 +0700

    update

--- src/uet/oop/bomberman/GUI.java
+++ src/uet/oop/bomberman/GUI.java
@@ -2,14 +2,11 @@ package uet.oop.bomberman;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
